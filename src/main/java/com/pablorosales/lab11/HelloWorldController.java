04b0722f05b14b97597cc47c1d49a07619213e12
pablo <pablo.rosales14@gmail.com>
2018-12-11 11:59:03 -0800

    got the capitalize route to work properly

--- src/main/java/com/pablorosales/lab11/HelloWorldController.java
+++ src/main/java/com/pablorosales/lab11/HelloWorldController.java
@@ -4,6 +4,7 @@ package com.pablorosales.lab11;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 //This for using the annotation @RestController:
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,6 +13,11 @@ public class HelloWorldController {
   @RequestMapping("/")
   public String helloWorld(){
     return "Hello World!";
+  }
+
+  @RequestMapping("/capitalize")
+  public String upperCaseMessage(@RequestParam(name = "name", required=false)String name){
+    return name.toUpperCase();
   }
 
 }
