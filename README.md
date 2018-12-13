4ebff2fe1674dc0f6ec6bd28c9b9fc221ee30fb2
pablo <pablo.rosales14@gmail.com>
2018-12-13 10:52:47 -0800

    This was supposed to be on branch day1, but too late for regrets

--- README.md
+++ README.md
@@ -6,6 +6,13 @@ Created by Pablo Rosales
 ## Capabilities
 This project as of right now creates a local server to handle the following two routes.
 
-1. ("/") The default route will message you a"basic "Hello World!" greeting.
+1. (/) The default route will message you a basic spring mvc error message.
 
-2. ("/capitalize") This requests a query parameter which will be returned to the user as a capitalized message.
+2. (/capitalize/) This takes in anything after the last slash and will be returned to the user as a capitalized message on the page.
+
+3. (/hello) This takes you to a static web page that returns a basic "Hello world!" web page. 
+
+### Day 1: Implemented the framework to basic routes
+* Made two RestControllers because I do not want templeting for the most basic pages, they are fine being implementing this way.
+
+### Day 2:
--- src/main/java/com/pablorosales/lab11/HelloWorldController.java
+++ src/main/java/com/pablorosales/lab11/HelloWorldController.java
@@ -1,24 +1,18 @@
 package com.pablorosales.lab11;
-import org.springframework.stereotype.Controller; //This allows thymeleaf templeting
-import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.stereotype.Controller; //This allows thymeleaf templeting
+//import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-//import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.bind.annotation.RestController; //This for using the annotation @RestController:
+//import org.springframework.web.bind.annotation.RequestParam; //Not entirely sure of this import, but better safe than sorry.
+import org.springframework.web.bind.annotation.RestController; //This for using the annotation @RestController:
 
-@Controller
+@RestController
 public class HelloWorldController {
 
   @RequestMapping(value="/hello", method=RequestMethod.GET)
-  public String helloWorld(){
-    return "Hello World!";
+  public static String helloWorld() {
+    return "Hello world!";
   }
 
-  @RequestMapping(value="/capitalize/${String}", method= RequestMethod.GET)
-  public String capitalize(@PathVariable()String string){return capitalizeString(string);}
-
-  public static String capitalizeString(String stringToCapitalize){
-    return stringToCapitalize.toUpperCase();
-  }
 
 }
